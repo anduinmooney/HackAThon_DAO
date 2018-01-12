c1a32c4e3fe9bd131d88804559ce4779c531c990
Anduin Mooney <anduinmooney@gmail.com>
2018-01-12 15:12:15 -0800

    Add templates.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import models.Team;
+import models.Member;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+
+import javax.print.DocFlavor;
+
+import static spark.Spark.*;
+
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        
+    }
 }
