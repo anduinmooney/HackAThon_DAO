90fda69cf36bb2dc264a02f52b1cfab2f4d707cb
Anduin Mooney <anduinmooney@gmail.com>
2018-01-12 17:02:37 -0800

    Update App.Java, fill templates. Finish README.md.

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
+# _Anduin's HackAThon 2018_
+
+#### _This application will allow the user to manage teams and members._
+
+#### By _**Anduin Mooney**_
+
+## Description
+
+_This application will allow HackAThon hosts to add and edit teams, along with its members._
+
+## Setup/Installation Requirements
+
+* _Clone repository from github_
+* _Launch application with IntelliJ_
+* _Run App.java_
+
+_Make sure to install Intellij or you won't be able to run this app._
+
+
 ### Specifications
 | Behavior | Input | Output |
 | :-------------     | :------------- | :-------------
@@ -7,3 +26,22 @@
 | User can add new team | "New Team" | "New Team"|
 | User can add team description| "Team Description"| "Team Description"|
 | User can add members to team| "Gabe" | "Add:Gabe" |
+
+
+## Known Bugs
+
+_Team name, description, and members fail to appear on the "See All Teams" tab, unable to edit teams. _
+
+## Support and contact details
+
+_If you have any issues with managing your HackAThon teams, please let me know at anduinmooney@gmail.com!_
+
+## Technologies Used
+
+_Java, IntelliJ, Atom. Gitbash, Spark, Handlebars_
+
+### License
+
+
+
+Copyright (c) 2018 **Anduin Mooney**
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.sun.org.apache.xpath.internal.operations.Mod;
@@ -18,6 +19,47 @@ public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
-        
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        get("teams/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "newTeam.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("teams/new/newMember",(request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String teamName = request.queryParams("name");
+            String description = request.queryParams("description");
+            Team newTeam = new Team(teamName, description);
+            model.put("newTeam", newTeam);
+            return new ModelAndView(model, "newMember.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("teams/new/newMember/next",(request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String memberName = request.queryParams("name");
+            Member newMember = new Member(memberName);
+            model.put("newMember", newMember);
+            return new ModelAndView(model, "newMember.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/teams/allteams", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            List allTeams = Team.getAllTeams();
+            model.put("teams", allTeams);
+            return new ModelAndView(model, "details.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/teams/details/finish", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            List teams = Team.getAllTeams();
+            model.put("teams", teams);
+            return new ModelAndView(model, "display.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 }
