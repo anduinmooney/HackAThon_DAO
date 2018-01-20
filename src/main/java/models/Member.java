d93f6e222b0714b79750d5d4050209767438e2cb
Anduin Mooney <anduinmooney@gmail.com>
2018-01-19 17:04:44 -0800

    Ignore bugfixing, add overrides for members.

--- src/main/java/models/Member.java
+++ src/main/java/models/Member.java
@@ -20,12 +20,30 @@ public class Member {
         return hackMemberName;
     }
 
-    public static List<Member> getAllMembers() {
-        return allMembers;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Member member = (Member) o;
+
+        if (id != member.id) return false;
+        if (hackMemberName != null ? !hackMemberName.equals(member.hackMemberName) : member.hackMemberName != null) return false;
+        return allMembers != null ? allMembers.equals(member.allMembers) : member.allMembers == null;
     }
 
+    @Override
+    public int hashCode() {
+        int result = hackMemberName != null ? hackMemberName.hashCode() : 0;
+        result = 31 * result + (allMembers != null ? allMembers.hashCode() : 0);
+        result = 31 * result + id;
+        return result;
+    }
+
+
+
     public int getMemberId() {
-        return hackMemberId;
+        return id;
     }
 
     public static void clearAllMembers() {
