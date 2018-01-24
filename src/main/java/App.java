6982f70b649574bce499f27a728bd4efdf87cc4b
Anduin Mooney <anduinmooney@gmail.com>
2018-01-24 09:44:00 -0800

    Uncomment App.java, update until team functionality works.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,78 +1,86 @@
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
-//
-//import com.sun.org.apache.xpath.internal.operations.Mod;
-//import models.Team;
-//import models.Member;
-//import spark.ModelAndView;
-//import spark.template.handlebars.HandlebarsTemplateEngine;
-//
-//
-//import javax.print.DocFlavor;
-//
-//import static spark.Spark.*;
-//
-//
-//public class App {
-//    public static void main(String[] args) {
-//        staticFileLocation("/public");
-//
-//        get("teams/new", (request, response) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            return new ModelAndView(model, "newTeam.hbs");
-//        }, new HandlebarsTemplateEngine());
-//
-//        post("teams/new/success",(request, response) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            String teamName = request.queryParams("teamName");
-//            String description = request.queryParams("teamDescription");
-//            String teamMate1 = request.queryParams("teamMate1");
-//            String teamMate2 = request.queryParams("teamMate2");
-//            String teamMate3 = request.queryParams("teamMate3");
-//            String teamMate4 = request.queryParams("teamMate4");
-//            String teamMate5 = request.queryParams("teamMate5");
-//            Team newTeam = new Team(teamName, description, teamMate1, teamMate2, teamMate3, teamMate4, teamMate5);
-//            model.put("newTeam", newTeam);
-//            return new ModelAndView(model, "success.hbs");
-//        }, new HandlebarsTemplateEngine());
-//
-//
-//        get("/", (request, response) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            return new ModelAndView(model, "index.hbs");
-//        }, new HandlebarsTemplateEngine());
-//
-//
-//        get("/teams/allteams", (request, response) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            List allTeams = Team.getAllTeams();
-//            model.put("allTeams", allTeams);
-//            return new ModelAndView(model, "details.hbs");
-//        }, new HandlebarsTemplateEngine());
-//
-//
-//        get("/teams/:id/update", (request, response) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            int idOfTeamToEdit = Integer.parseInt(request.params("id"));
-//            Team editTeam = Team.findById(idOfTeamToEdit);
-//            model.put("editTeam", editTeam);
-//            return new ModelAndView(model, "update.hbs");
-//        }, new HandlebarsTemplateEngine());
-//
-//        post("/teams/:id/update", (request, response) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            String newTeamName = request.queryParams("teamName");
-//            int idOfTeamToEdit = Integer.parseInt(request.params("id"));
-//            Team editTeam = Team.findById(idOfTeamToEdit);
-//            editTeam.update(newTeamName);
-//            return new ModelAndView(model, "success.hbs");
-//        }, new HandlebarsTemplateEngine());
-//
-//
-//
-//
-//
-//    }
-//}
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import dao.Sql2oTeamDao;
+import models.Team;
+import models.Member;
+import org.sql2o.Sql2o;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+
+import javax.print.DocFlavor;
+
+import static spark.Spark.*;
+
+
+public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        String connectionString = "jdbc:h2:~/todolist.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        Sql2oTeamDao teamDao = new Sql2oTeamDao(sql2o);
+
+
+        get("/teams/delete", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            teamDao.clearAllTeams();
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("teams/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "newTeam.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("teams/new/success",(request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String teamName = request.queryParams("teamName");
+            String description = request.queryParams("teamDescription");
+            Team newTeam = new Team(teamName, description);
+            teamDao.add(newTeam);
+            model.put("newTeam", newTeam);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        get("/teams/allteams", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            List allTeams = teamDao.getAll();
+            model.put("allTeams", allTeams);
+            return new ModelAndView(model, "details.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        get("/teams/:id/update", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfTeamToEdit = Integer.parseInt(request.params("id"));
+            Team editTeam = teamDao.findById(idOfTeamToEdit);
+            model.put("editTeam", editTeam);
+            return new ModelAndView(model, "update.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/teams/:id/update", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String newTeamName = request.queryParams("teamName");
+            int idOfTeamToEdit = Integer.parseInt(request.params("id"));
+            Team editTeam = teamDao.findById(idOfTeamToEdit);
+            editTeam.update(newTeamName);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+
+
+
+    }
+}
